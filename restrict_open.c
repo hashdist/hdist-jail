b2b8a99c4cbb66fd7653b8d60679ea0a8600b9d6
Ondrej Certik <ondrej@certik.cz>
2011-12-16 16:24:20 -0800

    Add abort()

--- restrict_open.c
+++ restrict_open.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 
 int open(const char *pathname, int flags, mode_t mode)
 {
-    printf("RESTRICT: %s: %s\n", __PRETTY_FUNCTION__, pathname);
+    printf("RESTRICTED OPEN: %s\n", pathname);
+    // You can abort using:
+    //abort();
     return syscall(SYS_open, pathname, flags, mode);
 }
